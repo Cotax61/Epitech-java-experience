7334e7dae696f4841aa1f7bc57bfc1a90d485805
Cotax <alexandre.descoins@epitech.eu>
2020-05-31 13:53:10 +0200

    [ADD]
    Categories for the shop (well... i don't know what to sell so it will still be a WIP)

--- CoQuest/src/fr/cotax/coquest/shop/ShopFrontPage.java
+++ CoQuest/src/fr/cotax/coquest/shop/ShopFrontPage.java
@@ -27,14 +27,29 @@ public class ShopFrontPage {
 		return (item);
 	}
 	
+	public ItemStack CreateShopItem(Material mat, String name)
+	{
+		ItemStack item = new ItemStack(mat);
+		ItemMeta meta = item.getItemMeta();
+		
+		meta.setDisplayName(name);
+		item.setItemMeta(meta);
+		return (item);		
+	}
+	
 	public void OpenShop(Player player)
 	{
 		Inventory menu = Bukkit.createInventory(null, 27, "§eBoutique §7| §6" + util.get_points(player) + "\u2726");
 
-		for (int i = 0; i != 10; i++)
+		for (int i = 0; i != 11; i++)
 			menu.setItem(i, CreateEmptyItem(Material.GRAY_STAINED_GLASS_PANE));
-		for (int i = 17; i != 27; i++)
+		for (int i = 16; i != 27; i++)
 			menu.setItem(i, CreateEmptyItem(Material.GRAY_STAINED_GLASS_PANE));
+		menu.setItem(11, CreateShopItem(Material.GOLDEN_SWORD, "§cEquipements"));
+		menu.setItem(12, CreateShopItem(Material.GOLDEN_CHESTPLATE, "§7Armures"));
+		menu.setItem(13, CreateShopItem(Material.EXPERIENCE_BOTTLE, "§1Enchantements"));
+		menu.setItem(14, CreateShopItem(Material.GRASS, "§aRessources"));
+		menu.setItem(15, CreateShopItem(Material.SPAWNER, "§6Spawners"));
 		player.openInventory(menu);
 	}
 }
