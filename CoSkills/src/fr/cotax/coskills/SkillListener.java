21c8f405e14e4f647bd03da6124c083b982a5825
Cotax <alexandre.descoins@epitech.eu>
2020-06-01 02:46:41 +0200

    [ADD]
    Registered all xp needed for all 100 levels of each skills

--- CoSkills/src/fr/cotax/coskills/SkillListener.java
+++ CoSkills/src/fr/cotax/coskills/SkillListener.java
@@ -3,13 +3,16 @@ package fr.cotax.coskills;
 import org.bukkit.event.Listener;
 
 import fr.cotax.coskills.sql.SqlSkillsUtilities;
+import fr.cotax.coskills.xp.XpNeeded;
 
 public class SkillListener implements Listener {
 	
 	public SqlSkillsUtilities util;
-	
+	public XpNeeded xp_need;
+
 	public SkillListener(SqlSkillsUtilities util) {
 		this.util = util;
+		this.xp_need = new XpNeeded();
 	}
 	
 	
